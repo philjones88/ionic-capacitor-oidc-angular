10c0d3cd5ad9b655f42ea564a62e1d7c568fd6ae
Phil Jones <pjones@vqcomms.com>
2018-09-25 16:59:19 +0100

    add xplat ionic app

---
+++ apps/ionic-sample/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// libs
+import { FooIonicCoreModule } from '@ionic-capacitor-oidc-angular/ionic';
+
+@NgModule({
+  imports: [FooIonicCoreModule]
+})
+export class CoreModule {}
